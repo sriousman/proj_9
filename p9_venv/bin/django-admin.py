0c8a1a2c0ba9fc9a392c4c2e0bd76e5ff5ad364f
Gregory Covington <greg@virulantconsulting.com>
2018-01-05 13:52:00 -0600

    init and changed req.txt to get debug-toolbar working

---
+++ p9_venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/srious/Virulant/techdegree/current/improve_django_v3/p9_venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
